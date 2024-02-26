1eca20099f1f5bef1639fb842122f82643d62eff
F5RHH <127747816+F5RHH@users.noreply.github.com>
2024-02-26 17:53:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "git@github.com:F5RHH/MUSIC.git",
+    "https://github.com/F5RHH/MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
